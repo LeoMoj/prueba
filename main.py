42609935b851b659664dde89a31345685193e9ba
LeoMoj <leonardo.mojica@softtek.com>
2023-11-19 13:33:05 -0600

    Multi-line comments were created for all the functions of the project explaining what they do.

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 
-
+def prueba(x:int, y:int):
+    """
+    This function adds two integers.
+    
+    Cyclomatic Complexity: 1
+    
+    Args:
+      x (int): the first number to be added
+      y (int): the second number to be added
+    
+    Returns:
+       int: the sum of x and y
+    """
+    return x + y
 
 if __name__ == "__main__":
     print("hola")
