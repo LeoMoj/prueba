2d7b398c81da00331c4c19e1b67cc96dcef2bd04
LeoMoj <leonardo.mojica@softtek.com>
2023-11-20 16:31:20 -0600

    p

--- main.py
+++ main.py
@@ -1,19 +1,6 @@
 
 def prueba(x:int, y:int):
     #cambio extraño para probar
-    """
-    This function adds two integers.
-    
-    Cyclomatic Complexity: 1
-    
-    Args:
-      x (int): the first number to be added
-      y (int): the second number to be added
-    
-    Returns:
-       sum (int): the sum of x and y
-    """
-
     return x + y
 
 if __name__ == "__main__":
